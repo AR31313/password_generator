76cf06b02eedfe2127a63be739c9ecdb1dc3716c
AR31313 <gurlgotgame@gmail.com>
2022-03-09 11:09:43 -0800

    working code

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+
+// var arr = 10;
+// for (let i = 0; i < arr; i++) {
+//   console.log(arr[i]);
+//   console.log(arr)
+// }
+
+// var value = "6";
+// if(value === 5){
+//   console.log(typeof(value))
+// }else if(value === "5"){
+//   console.log(typeof(value))
+// } else {
+//   console.log("you didnt even define it")
+// }
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,16 +29,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  //get the preference 
-  //identify & collect preference the user has chosen (1 or 4 chosen scopes)
-  //create a collection with the chosen scopes,locally scored temp array
-  //create a password variable, an array
-  //randomly draw character
-  //put the character on the generatePasszVCword variable
-  //repeat using forloops, as many times as specified.
-  //loop through garanteed elements, replace elements in pasword array. (Math.Random)
-  //join chraccters in the password array into a string.
-  //return the password string
+
 
 //declared in the global scope and is available to all functions 
   var num = [0,1,2,3,4,5,6,7,8,9];
@@ -29,31 +37,55 @@ generateBtn.addEventListener("click", writePassword);
   var lower = ["a","b","c","e","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var specialChars =["!","@","#","$","%","^","&","*","_","+","~","<",">","?","/"];
   var useLength;
-  let randomPassword;
-  var useUpper;
-  var useLower;
-  var usenum;
-  var usespChar;
-
+  let randomPassword =[];
+  var userchoice = [];
 
 //a function named generatePassword
 function generatePassword (){
   var useLength = parseInt (prompt ("How many characters would you like?"))
+  console.log (typeof useLength)
   if (useLength <8 || useLength > 128){
     alert ("Use a passphrase between 8 & 128 Characters.")
     return null
   }
- 
+  var usenum = confirm ("Would you like to include numbers?");
   var useUpper = confirm ("Would you like to include uppercase?");
   var useLower = confirm ("Would you like to include lowercase?");
   var usespChar = confirm ("Would you like to include special characters?");
-  var usenum = confirm ("Would you like to include numbers?");
 
-let randomPassword = " ";
-  for (let i = 0; i < useLength; i++) {
-  randomPassword += masterArray[Math.floor(Math.random() * (masterArray.length))];
-  console.log(Math.floor(Math.random() * (masterArray.length)));
-  console.log(randomPassword);
+  //identify & collect preference the user has chosen (1 or 4 chosen scopes)
+  console.log (userchoice);
+  if (usenum ==true){
+    userchoice=userchoice.concat (num);
+    // userchoice+=num
   }
-  return randomPassword;
+  console.log (userchoice);
+  if (useUpper ==true){
+    userchoice=userchoice.concat(upper);
+  }
+  console.log (userchoice);
+  if (useLower ==true){
+    userchoice=userchoice.concat(lower);
+  }
+  console.log (userchoice);
+  if (usespChar ==true){
+    userchoice=userchoice.concat(specialChars);
+  }
+
+// empty string variable for the for loop below
+var randomPasswordGenerated = [];
+
+ // loop getting random characters
+ for (var i = 0; i < useLength; i++) {
+  var randomNumberPicked = Math.floor(Math.random() * (userchoice.length + 1));
+  //join chraccters in the password array into a string.
+  randomPasswordGenerated= randomPasswordGenerated + userchoice[randomNumberPicked] 
+// console.log (randomPasswordGenerated);
+// console.log(useLength);
+// console.log (randomNumberPicked);
+// console.log (i);
+ 
+}
+//return the password string
+return randomPasswordGenerated;
 }
